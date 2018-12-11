de81a412a4685ac442c71158824eac75fbdb4937
michelspirlandeli <michelspirlandeli10@gmail.com>
2018-12-11 09:47:35 -0200

    Connect mongodb with repository and service

---
+++ src/main/java/com/spring/mongodb/service/UserService.java
@@ -0,0 +1,20 @@
+package com.spring.mongodb.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.spring.mongodb.domain.User;
+import com.spring.mongodb.repository.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	public List<User> findAll(){
+		return userRepository.findAll();
+	}
+}
