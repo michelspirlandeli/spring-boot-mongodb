a61cc801f0ce2631c6081f01fdfad1f610a50361
michelspirlandeli <michelspirlandeli10@gmail.com>
2018-12-28 11:03:43 -0200

    Create post and user

---
+++ src/main/java/com/spring/mongodb/repository/PostRepository.java
@@ -0,0 +1,12 @@
+package com.spring.mongodb.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.spring.mongodb.domain.Post;
+
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+
+}
