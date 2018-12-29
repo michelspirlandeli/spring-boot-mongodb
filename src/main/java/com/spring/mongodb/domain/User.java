5121dc388eb1f64c1f6540d4490c1eac318a00ca
michelspirlandeli <michelspirlandeli10@gmail.com>
2018-12-29 13:58:22 -0200

    referencing the user post

--- src/main/java/com/spring/mongodb/domain/User.java
+++ src/main/java/com/spring/mongodb/domain/User.java
@@ -1,9 +1,14 @@
 package com.spring.mongodb.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+import com.spring.mongodb.domain.Post;
 
 @Document(collection="user")
 public class User implements Serializable{
@@ -14,6 +19,9 @@ public class User implements Serializable{
 	private String id;
 	private String name;
 	private String email;
+	
+	@DBRef(lazy = true)
+	private List<Post> posts = new ArrayList<>();
 	
 	public User() {
 		
@@ -48,6 +56,16 @@ public class User implements Serializable{
 
 	public void setEmail(String email) {
 		this.email = email;
+	}
+	
+	
+
+	public List<Post> getPosts() {
+		return posts;
+	}
+
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
 	}
 
 	@Override
